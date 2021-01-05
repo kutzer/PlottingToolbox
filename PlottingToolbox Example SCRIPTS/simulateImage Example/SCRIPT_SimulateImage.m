4b5728d84bdd9e901563060e65218f060e64c2e4
Michael Kutzer <kutzer@usna.edu>
2021-01-05 12:53:19 -0500

    Simulate image example

--- PlottingToolbox Example SCRIPTS/simulateImage Example/SCRIPT_SimulateImage.m
+++ PlottingToolbox Example SCRIPTS/simulateImage Example/SCRIPT_SimulateImage.m
@@ -10,17 +10,27 @@ fig = open('Wall-E.fig');
 
 %% Get axes object
 axs = get(fig,'Children');
+triad('Parent',axs,'Scale',100,'LineWidth',2);
 
 %% Define image dimensions & dpi
 vpix = 480;
 hpix = 640;
 
 %% Define extrinsic matrix
-H_p2m = Tx(120)*Ty(15)*Tz(1000)*Rx(pi/3)*Rz(pi/6);
+H_p2m = Tx(50)*Ty(15)*Tz(800)*Rx(pi/3)*Rz(pi/6);
 
 %% Simulate image
 im = simulateImage(axs,cameraParams,H_p2m,vpix,hpix);
 
 %% Show image
 figure;
-imshow(im);
+img = imshow(im);
+
+%% Move the robot around it's body-fixed z
+thetas = linspace(0,2*pi,100);
+for theta = thetas
+    H_p2m_now = H_p2m*Rz(theta);
+    im = simulateImage(axs,cameraParams,H_p2m_now,vpix,hpix);
+    set(img,'CData',im);
+    drawnow;
+end
