0c3c31d8c355df3518f18bdd51469f5743384a3f
Michael Kutzer <kutzer@usna.edu>
2021-08-02 12:02:52 -0400

    Smooth path

--- PlottingToolbox Example SCRIPTS/SCRIPT_UR3e_Write.m
+++ PlottingToolbox Example SCRIPTS/SCRIPT_UR3e_Write.m
@@ -21,6 +21,15 @@ H_p2c = Tx(x_c)*Ty(y_c)*Tz(z_c);
 X(4,:) = 1;
 X_c = H_p2c * X;
 
+%% Plot position and velocity
+t = 0:dt:(size(X_c,2)-1)*dt;
+fig = figure;
+axs = axes('Parent',fig);
+hold(axs,'on')
+plot(axs,t,X_c(1,:),'r');
+plot(axs,t,X_c(2,:),'g');
+plot(axs,t,X_c(3,:),'b');
+
 %% Create simulation 
 sim = URsim;
 sim.Initialize('UR3');
--- PlottingToolboxFunctions/pShapesToSplines.m
+++ PlottingToolboxFunctions/pShapesToSplines.m
@@ -26,6 +26,8 @@ function [pps,sEnds] = pShapesToSplines(pShapes)
 %
 %   M. Kutzer, 28Jul2021, USNA
 
+plotsON = false;
+
 %% Check input(s)
 switch lower( class(pShapes) )
     case 'polyshape'
@@ -34,25 +36,75 @@ switch lower( class(pShapes) )
         error('Input must be an array of one or more polyshapes.');
 end
 
+%% Setup debug plot(s)
+if plotsON
+    fig = figure;
+    axs = axes('Parent',fig);
+    hold(axs,'on');
+    daspect(axs,[1 1 1]);
+    pltP = plot(axs,0,0,'k');
+    pltV = plot(axs,0,0,'g');
+    pltS = plot(axs,0,0,'m');
+end
+
 %% Fit splines
-for i = 1:numel(pShapes)
+n = numel(pShapes);
+for i = 1:n
     pShape = pShapes(i);            % Isolate ith polyshape
     b = numboundaries( pShape );    % Identify number of boundaries 
     
     % Isolate vertices for each boundary
     for j = 1:b
         [x,y] = boundary(pShape,j);
+        % Combine terms
+        X = [x,y].'; 
+        % Approximate vectors
+        dXdk = diff(X,1,2);
         % Estimate arc length
-        ds = sqrt( diff(x).^2 + diff(y).^2 );
-        ds = [0; ds];
+        ds = sqrt( sum(dXdk.^2,1) );
+        ds = [0, ds];
         s = cumsum(ds);
+        % Estimate velocities
+        dXdk_hat = dXdk./ds(2:end);
+        % OPTION 1: Average enter/exit velocities
+        dXds = [...
+            dXdk_hat(:,1),...                                  % Vector for first
+            (dXdk_hat(:,1:(end-1)) + dXdk_hat(:,2:end))./2,... % Vectors for intermediate(s)
+            dXdk_hat(:,end)];                                  % Vector for last
+        % OPTION 2: Use exit velicities
+        %{
+        dXds = [...
+            dXdk_hat(:,1),...       % Vector for first
+            dXdk_hat(:,1:(end))];  % Vectors for intermediate(s)
+        %}
         
+        % Apply unit vector requirement
+        dXds = dXds./sqrt( sum(dXds.^2,1) );
+        
+        % Fit splines
         if (i == 1) && (j == 1)
-            pps = fitpp(s.', [x,y].');
+            pps = fitpp(s,X,s,dXds);
             sEnds = s(end);
         else
-            pps(end+1) = fitpp(s.', [x,y].');
+            pps(end+1) = fitpp(s,X,s,dXds);
             sEnds(end+1) = s(end);
+        end
+        
+        % Debug plot
+        if plotsON
+            title(axs,sprintf('Polynomial %d of %d, Curve %d of %d',i,n,j,b));
+            ss = linspace(0,sEnds(end),5000);
+            XX = ppval(pps(end),ss);
+            set(pltP,'XData', X(1,:),'YData', X(2,:));
+            set(pltS,'XData',XX(1,:),'YData',XX(2,:));
+            delete(pltV);
+            for k = 1:size(dXds,2)
+                pltV(k) = plot(axs,...
+                    [X(1,k), X(1,k)+dXds(1,k)],...
+                    [X(2,k), X(2,k)+dXds(2,k)],'r');
+            end
+            drawnow
+            pause;
         end
     end
 end
