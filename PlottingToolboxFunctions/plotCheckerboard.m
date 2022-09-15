70d1dcecdeea214fca0d71a15271128d8c25b87b
Michael Kutzer <kutzer@usna.edu>
2022-09-15 14:36:18 -0400

    Update plotCheckerboard.m

--- PlottingToolboxFunctions/plotCheckerboard.m
+++ PlottingToolboxFunctions/plotCheckerboard.m
@@ -1,8 +1,8 @@
-function [hg, ptc] = plotCheckerboard(varargin)
+function [hg,ptc,cInfo] = plotCheckerboard(varargin)
 % PLOTCHECKERBOARD plots a visual representation of a checkerboard given a
 % 1x2 board size representing the number of squares in body-fixed the 
 % x-direction and y-direction, and the square size.
-%   [hg, ptcs] = plotCheckerboard(boardSize,squareSize) returns an
+%   [hg, ptcs, cInfo] = plotCheckerboard(boardSize,squareSize) returns an
 %   hgtransform (hg) object that is the parent of the checkerboard
 %   visualization, and the patch objects of each square (ptcs).
 %
@@ -29,6 +29,11 @@ function [hg, ptc] = plotCheckerboard(varargin)
 %   Output(s)
 %        hg - hgtransform object parent of the checkerboard patch objects
 %       ptc - patch objects visualizing the checkerboard
+%     cInfo - structured array containing checkerboard information
+%       cInfo.BoardSize  - 1x2 array defining board size
+%       cInfo.SquareSize - scalar value defining square size
+%       cInfo.Boundary   - 4x2 array containing the x/y bounding box of the
+%                          checkerboard
 %
 %   See also detectCheckerboardPoints generateCheckerboardPoints
 %
@@ -38,7 +43,7 @@ function [hg, ptc] = plotCheckerboard(varargin)
 %   14Apr2022 - Updated documentation
 %   14Apr2022 - Updated input parsing and added input checking
 %   14Apr2022 - Deleted copyobj patch after use
-
+%   15Sep2022 - Added cInfo output
 %% Parse inputs and set defaults 
 % TODO - check inputs and better check number of inputs
 narginchk(2,4);
@@ -100,6 +105,7 @@ p0 = patch(p0,'Parent',hg,'EdgeColor','k','FaceColor','w');
 xColor = false;
 x0 = -squareSize;
 y0 = -squareSize;
+xy = [];
 for i = 1:boardSize(2)      % boxes in x-direction
     % Define x-position of box
     x = x0 + (i-1)*squareSize;
@@ -123,6 +129,9 @@ for i = 1:boardSize(2)      % boxes in x-direction
         v(4,:) = [];
         ptc(i,j).Vertices = v.';
         
+        % Append new vertices
+        xy = [xy; v(:,1:2)];
+
         % Update face color
         if xyColor
             % "Black"
@@ -134,3 +143,17 @@ for i = 1:boardSize(2)      % boxes in x-direction
     end
 end
 delete(p0);
+
+%% Define cInfo
+if nargout > 2
+    xx = [min(xy(:,1)), max(xy(:,1))];
+    yy = [min(xy(:,2)), max(xy(:,2))];
+
+    cInfo.BoardSize  = boardSize;
+    cInfo.SquareSize = squareSize;
+    cInfo.Boundary   = [...
+        xx(1), yy(1);...
+        xx(1), yy(2);...
+        xx(2), yy(2);...
+        xx(2), yy(1)];
+end
