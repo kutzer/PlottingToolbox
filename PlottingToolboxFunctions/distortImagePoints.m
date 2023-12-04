c7ee4f8f1789b6a8a9de313ef70d0bba445787f6
Michael Kutzer <kutzer@usna.edu>
2023-12-04 15:13:02 -0500

    Update distortImagePoints.m

--- PlottingToolboxFunctions/distortImagePoints.m
+++ PlottingToolboxFunctions/distortImagePoints.m
@@ -36,6 +36,65 @@ function [Xd,axs] = distortImagePoints(Xu,params)
 %   18Feb2016 - Updated to accept and parse MATLAB camera parameters
 %   21Nov2023 - Updated to follow MATLAB documentation
 
+%% EMBEDDED MATLAB FUNCTION
+%{
+function distortedPoints = distortPoints(points, K, radialDistortion, tangentialDistortion)
+%
+
+%   Copyright 2014-2022 The MathWorks, Inc.
+
+%#codegen
+
+% unpack the pre-multiply intrinisc matrix
+cx = K(1, 3);
+cy = K(2, 3);
+fx = K(1, 1);
+fy = K(2, 2);
+skew = K(1, 2);
+
+% center the points
+center = [cx, cy];
+centeredPoints = bsxfun(@minus, points, center);
+
+% normalize the points
+yNorm = centeredPoints(:, 2, :) ./ fy;
+xNorm = (centeredPoints(:, 1, :) - skew * yNorm) ./ fx; % Distortion must be computed without skew
+
+% compute radial distortion
+r2 = xNorm .^ 2 + yNorm .^ 2;
+r4 = r2 .* r2;
+r6 = r2 .* r4;
+
+k = zeros(1, 3, 'like', radialDistortion);
+k(1:2) = radialDistortion(1:2);
+if numel(radialDistortion) < 3
+    k(3) = 0;
+else
+    k(3) = radialDistortion(3);
+end
+
+alpha = k(1) * r2 + k(2) * r4 + k(3) * r6;
+
+% compute tangential distortion
+p = tangentialDistortion;
+xyProduct = xNorm .* yNorm;
+dxTangential = 2 * p(1) * xyProduct + p(2) * (r2 + 2 * xNorm .^ 2);
+dyTangential = p(1) * (r2 + 2 * yNorm .^ 2) + 2 * p(2) * xyProduct;
+
+% apply the distortion to the points
+normalizedPoints = [xNorm, yNorm];
+distortedNormalizedPoints = normalizedPoints + normalizedPoints .* [alpha, alpha] + ...
+    [dxTangential, dyTangential];
+
+% convert back to pixels
+distortedPointsX = distortedNormalizedPoints(:, 1, :) * fx + cx + ...
+    skew * distortedNormalizedPoints(:, 2, :); % Add skew effect back
+distortedPointsY = distortedNormalizedPoints(:, 2, :) * fy + cy;
+
+
+distortedPoints = [distortedPointsX, distortedPointsY];
+%}
+
 debugOn = true;
 
 %% Check input(s)
